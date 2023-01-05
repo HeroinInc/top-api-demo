e4fc07857f4f124e14bd3379671b14e58e871233
HeroinInc <nakresaideliona@gmail.com>
2023-01-05 21:53:21 +0200

    product controller review controller mongo aggregation example

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,33 +1,68 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Inject,
+	NotFoundException,
+	Param,
+	Patch,
+	Post,
+	UsePipes, ValidationPipe,
+} from '@nestjs/common';
 import { ProductModel } from './product.model/product.model';
 import { FindProductDto } from './dto/find-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductService } from './product.service';
+import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
 
 @Controller('product')
 export class ProductController {
-
+	constructor(
+		@Inject(ProductService) private readonly productService: ProductService
+	) {
+	}
 	@Post('create')
-	async create(@Body() dto: Omit<ProductModel, '_id'>) {
-
+	async create(@Body() dto: CreateProductDto) {
+		return this.productService.create(dto)
 	}
 
 	@Get(':id')
 	async get(@Param('id') id: string){
+		const product = await this.productService.findById(id)
 
+		if (!product) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR)
+		}
+
+		return product
 	}
 
 	@Delete(':id')
 	async delete(@Param('id') id: string) {
+		const deleted = await this.productService.deleteById(id)
 
+		if (!deleted) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR)
+		}
 	}
 
 	@Patch(':id')
 	async patch(@Param('id') id: string, @Body() dto: ProductModel) {
+		const updated = await this.productService.updateById(id, dto)
 
+		if (!updated) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR)
+		}
+
+		return updated
 	}
 
+	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
-	@Post()
+	@Post('find')
 	async find(@Body() dto: FindProductDto){
-
+		return this.productService.findWithReviews(dto)
 	}
 }
